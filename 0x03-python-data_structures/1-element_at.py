6f17e64ac3c043095c7013501765a3a0036e74a6
flovian <flovian.owiti@gmail.com>
2022-06-06 12:54:25 +0000

    Task 1 correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,9 @@
 
 def element_at(my_list, idx):
     """Retrive an element from a list."""
-    if idx < 0 or idx > (len(my_list) - 1):
+    if idx < 0:
         return None
-    return (my_list[idx]
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
