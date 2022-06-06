6fa0ab69eeb811c784226ab1662b1803ee15555d
flovian <flovian.owiti@gmail.com>
2022-06-06 13:01:16 +0000

    Task 6 correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,5 @@
 
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for row in matrix:
+        print(" ".join("{:d}".format(el) for el in row))
