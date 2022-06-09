9c88c01c4b3ac1ccd855a58c69126b2fe1171c24
flovian <flovian.owiti@gmail.com>
2022-06-09 07:19:48 +0000

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
