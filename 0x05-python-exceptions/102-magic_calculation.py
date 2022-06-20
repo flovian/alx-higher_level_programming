c51b47640768d7cfa5631e250b9bcd0b91aa5ba8
flovian <flovian.owiti@gmail.com>
2022-06-20 21:22:53 +0000

    Fix task 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -13,7 +13,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except(ValueError, TypeError):
+        except BaseException:
             result = b + a
             break
     return result
