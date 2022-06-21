20f713aee02506501be0c3d5d545418eef3df959
flovian <flovian.owiti@gmail.com>
2022-06-21 19:08:16 +0000

    Fix advanced task

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,76 +1,60 @@
 #!/usr/bin/python3
-"""Define a class Square.
-Represent a square.
-Initialize a new square.
-Args:
-    size (int): The size of the new square.
-    position (int, int): The position of the new square.
-
-Get/set the current size of the square.
-"""
+# 6-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
-class Node:
+class Square:
+    """Represent a square."""
 
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next_node = next_node
-
-    @property
-    def data(self):
-        return (self.__data)
-
-    @data.setter
-    def data(self, value):
-        if not isinstance(value, int):
-            raise TypeError("dmust be an integer")
-        self.__data = value
-
-    @property
-    def next_node(self):
-        return (self.__next_node)
-
-    @next_node.setter
-    def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("must be a Node object")
-        self.__next_node = value
-
-
-class SinglyLinkedList:
-    """Represent a singly-linked list."""
-
-    def __init__(self):
-        """Initalize a new SinglyLinkedList."""
-        self.__head = None
-
-    def sorted_insert(self, value):
-        """Insert a new Node to the SinglyLinkedList.
-        The node is inserted into the list at the correct
-        ordered numerical position.
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
         Args:
-            value (Node): The new Node to insert.
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        new = Node(value)
-        if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+        self.size = size
+        self.position = position
 
-    def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
