ea69886c77563ea196932fbade4cf1cfdbb939a3
flovian <flovian.owiti@gmail.com>
2022-07-05 22:19:55 +0000

    Fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-'Log Parsing'
+'''
+Script for log parsing
+'''
 import sys
 
-status_list = [200, 301, 400, 401, 403, 404, 405, 500]
-try:
-    total_size = 0
-    final_list = []
-    for index, line in enumerate(sys.stdin, 1):
-        if line:
-            line_split = line.split()
-            if len(line_split) > 2:
-                if line_split[-1].isnumeric() and line_split[-2].isnumeric():
-                    size = line_split[-1]
-                    status = line_split[-2]
-                    total_size += int(size)
-            if len(status) > 0 and int(status) in status_list:
-                final_list.append(int(status))
-        if index % 10 == 0:
-            print('File size: {}'.format(total_size))
-            for i in status_list:
-                if i in final_list:
-                    status_count = final_list.count(i)
-                    print("{}: {}".format(i, status_count))
-except Exception:
-    pass
 
-finally:
-    print('File size: {}'.format(total_size))
-    for i in status_list:
-        if i in final_list:
-            status_count = final_list.count(i)
-            print("{}: {}".format(i, status_count))
+if __name__ == "__main__":
+    a = []
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    dictio = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+              "405": 0, "500": 0}
+    i = 0
+    file_size = 0
+    try:
+        for lines in sys.stdin:
+            a = lines.split(" ")
+            if "\n" in a[-1]:
+                value = a[-1][:-1]
+            else:
+                value = a[-1]
+            if len(a) > 2 and a[-2] in codes and value.isnumeric() is True:
+                dictio[a[-2]] += 1
+                file_size += int(value)
+                i += 1
+            if i % 10 == 0:
+                print("File size: {}".format(file_size))
+                for code in codes:
+                    if dictio[code] > 0:
+                        print("{}: {}".format(code, dictio[code]))
+        exit()
+    except Exception:
+        pass
+    finally:
+        print("File size: {}".format(file_size))
+        for code in codes:
+            if dictio[code] > 0:
+                print("{}: {}".format(code, dictio[code]))
